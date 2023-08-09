195879c40e0ce7a3af64bba85372753774073d72
HenningCV <65901747+HenningCV@users.noreply.github.com>
2023-08-09 12:07:14 +0200

    created game model

---
+++ app/src/main/java/de/collector/model/Game.java
@@ -0,0 +1,65 @@
+package de.collector.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+@Entity
+public class Game {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "imagePath")
+    private String imagePath;
+
+    @NonNull
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @NonNull
+    @ColumnInfo(name = "publisher")
+    private String publisher;
+
+    public Game(int id, @NonNull String imagePath, @NonNull String title, @NonNull String publisher) {
+        this.id = id;
+        this.imagePath = imagePath;
+        this.title = title;
+        this.publisher = publisher;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @NonNull
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(@NonNull String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NonNull String title) {
+        this.title = title;
+    }
+
+    @NonNull
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(@NonNull String publisher) {
+        this.publisher = publisher;
+    }
+}
