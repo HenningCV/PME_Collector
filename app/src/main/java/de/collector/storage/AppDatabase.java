6806e8d064377af244850ebab55506d63814a3bc
HenningCV <65901747+HenningCV@users.noreply.github.com>
2023-08-09 13:14:42 +0200

    Create AppDatabase.java

---
+++ app/src/main/java/de/collector/storage/AppDatabase.java
@@ -0,0 +1,59 @@
+package de.collector.storage;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.Collections;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import de.collector.model.Game;
+import de.collector.model.Item;
+
+@Database(entities = {Game.class, Item.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract GameDAO gameDAO();
+    public abstract ItemDAO itemDAO();
+
+    private static final int NUMBER_OF_THREADS = 4;
+    private static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool( NUMBER_OF_THREADS );
+
+    private static volatile AppDatabase INSTANCE;
+
+    // Create and open database
+    static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "app_db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public static <T> T query(Callable<T> task)
+            throws ExecutionException, InterruptedException
+    {
+        return databaseWriteExecutor.invokeAny( Collections.singletonList( task ) );
+    }
+
+    public static void execute( Runnable runnable )
+    {
+        databaseWriteExecutor.execute( runnable );
+    }
+
+
+
+
+
+}
