a678de752aa1d977ba473c7e54077c891b231803
Lucas / Diafreak <lucas.hengelhaupt@fh-erfurt.de>
2023-08-29 21:18:34 +0200

    implemented light & dark theme toggle

--- app/src/main/java/de/pme/collector/view/MainActivity.java
+++ app/src/main/java/de/pme/collector/view/MainActivity.java
@@ -1,11 +1,15 @@
 package de.pme.collector.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,17 +18,55 @@ import de.pme.collector.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        // get SharedPreferences instance
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setTheme();
+
+        setContentView(R.layout.activity_main);
 
         // set up bottom navigation
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation_view);
 
+        // set up navigation-controller
         NavController navController = Navigation.findNavController(this, R.id.navigation_home);
 
+        // set up bottom navigation with navigation-controller
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+
+    private void setTheme() {
+        // set default theme to night-mode
+        boolean isDarkModeEnabled = sharedPreferences.getBoolean("dark_mode", true);
+
+        if (isDarkModeEnabled) {
+            // dark theme
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else {
+            // light theme
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        // update dark-mode switch state
+        updateDarkModeSwitchPreference(isDarkModeEnabled);
+    }
+
+
+    private void updateDarkModeSwitchPreference(boolean isDarkModeEnabled) {
+        // update dark mode switch preference value
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putBoolean("dark_mode", isDarkModeEnabled);
+
+        editor.apply();
     }
 }
--- app/src/main/java/de/pme/collector/view/fragments/settings/SettingsFragment.java
+++ app/src/main/java/de/pme/collector/view/fragments/settings/SettingsFragment.java
@@ -1,15 +1,71 @@
 package de.pme.collector.view.fragments.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import de.pme.collector.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        // register the listener for preference changes
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+
+        assert sharedPreferences != null;
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        // dark-mode switch
+        if (key.equals("dark_mode")) {
+
+            boolean isDarkModeEnabled = sharedPreferences.getBoolean(key, false);
+
+            updateThemeSwitch(isDarkModeEnabled);
+
+            if (isDarkModeEnabled) {
+                // dark theme
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            else {
+                // light theme
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+
+            // recreate activity to apply theme changes
+            requireActivity().recreate();
+        }
+    }
+
+
+    private void updateThemeSwitch(boolean darkModeEnabled) {
+
+        SwitchPreferenceCompat darkModeSwitch = findPreference("dark_mode");
+
+        assert darkModeSwitch != null;
+        darkModeSwitch.setChecked(darkModeEnabled);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+
+        // unregister the listener to prevent memory leaks
+        assert sharedPreferences != null;
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 }
